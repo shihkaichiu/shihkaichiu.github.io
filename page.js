aaba290f8e0fe6e69b39cce7117d18fb0359425a
Shih-Kai Chiu <shihkaichiu@gmail.com>
2021-11-28 02:12:14 +0000

    detect dark mode; seminar details

--- page.js
+++ page.js
@@ -20,18 +20,38 @@ function toggleFace(x) {
     }
 }
 
+const prefersDarkScheme = window.matchMedia('(prefers-color-scheme: dark)');
+
+window.onload = function() {
+    if (prefersDarkScheme.matches) {
+        // dark mode
+        var btn = document.getElementById("darkModeButton");
+        btn.innerHTML = "Light mode";
+    }
+}
 
 function toggleInvert() {
-    btn = document.getElementById("darkModeButton");
-
-    if (btn.innerHTML == "Dark mode") {
-        document.body.classList.add("dark");
-        document.querySelectorAll("a").forEach(l => l.classList.add("dark"));
+    var btn = document.getElementById("darkModeButton");
+    if (prefersDarkScheme.matches) {
+        if (btn.innerHTML == "Dark mode") {
+            document.body.classList.remove("light");
+            document.querySelectorAll("a").forEach(l => l.classList.remove("light"));
+        }
+        else {
+            document.body.classList.add("light");
+            document.querySelectorAll("a").forEach(l => l.classList.add("light"));
+        }
     }
     else {
-        var elements = document.getElementsByClassName('dark');
-        while(elements.length > 0){
-            elements[0].classList.remove('dark');
+        if (btn.innerHTML == "Dark mode") {
+            document.body.classList.add("dark");
+            document.querySelectorAll("a").forEach(l => l.classList.add("dark"));
+        }
+        else {
+            var elements = document.getElementsByClassName('dark');
+            while(elements.length > 0){
+                elements[0].classList.remove('dark');
+            }
         }
     }
 
